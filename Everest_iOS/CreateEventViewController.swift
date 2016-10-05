2d9a24caa44068e081ac551f229f4d3d008530e2
Sebastian <sebastian.kolosa@gmail.com>
2016-10-04 23:53:54 -0400

    begin laying out view and controller classes

---
+++ Everest_iOS/CreateEventViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateEventViewController.swift
+//  Everest_iOS
+//
+//  Created by Sebastian Kolosa on 2016-10-04.
+//  Copyright © 2016 Everest. All rights reserved.
+//
+
+import UIKit
+
+class CreateEventViewController: InputTableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let createEventViewContainer = CreateEventViewContainer()
+        createEventViewContainer.contentTableview?.delegate = self
+        
+        self.view.addSubview(createEventViewContainer)
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 7
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "testCell")
+        cell.backgroundView?.backgroundColor = UIColor.black
+        return cell
+    }
+}
