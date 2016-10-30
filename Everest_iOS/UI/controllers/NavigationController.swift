5175678a47d22035648e1db1c1ce9110c5c290b3
Sebastian <sebastian.kolosa@gmail.com>
2016-10-30 17:28:50 -0400

    IOS-16 rebase conflict fix 1

---
+++ Everest_iOS/UI/controllers/NavigationController.swift
@@ -0,0 +1,14 @@
+//
+//  NavigationController.swift
+//  Everest_iOS
+//
+//  Created by Sebastian Kolosa on 2016-10-29.
+//  Copyright © 2016 Everest. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NavigationController: UIViewController {
+    
+}
---
+++ Everest_iOS/UI/views/navigation/ToolBar.swift
@@ -0,0 +1,13 @@
+//
+//  ToolBar.swift
+//  Everest_iOS
+//
+//  Created by Sebastian Kolosa on 2016-10-29.
+//  Copyright © 2016 Everest. All rights reserved.
+//
+
+import UIKit
+
+class ToolBar: UIToolbar {
+    
+}
