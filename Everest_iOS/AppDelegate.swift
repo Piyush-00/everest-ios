feadd69bfa751dfabb3e2f4d3df5d79d491d45f7
Sebastian <sebastian.kolosa@gmail.com>
2016-10-30 18:14:26 -0400

    IOS-19 refactor landing page vc for navigation controller

--- Everest_iOS/AppDelegate.swift
+++ Everest_iOS/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         
-        let rootView = SignUpViewController()
+        let rootView = LandingViewController()
         
         navigationController = NavigationController()
         navigationController?.navigationBar.isHidden = true
