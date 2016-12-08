f9f50eb06cefe1985f28e5e6b2a1d4cd62eca44f
Sebastian <sebastian.kolosa@gmail.com>
2016-12-07 23:07:52 -0500

    IOS-36 add EventTabBarVC class

---
+++ Everest_iOS/UI/controllers/EventTabBarViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EventTabBarViewController.swift
+//  Everest_iOS
+//
+//  Created by Sebastian Kolosa on 2016-12-07.
+//  Copyright © 2016 Everest. All rights reserved.
+//
+
+import UIKit
+
+class EventTabBarViewController: UITabBarController {
+  override init(
+}
