aa0dc4235f56614eac6c4ed74e78e9215e19b0b0
Sebastian <sebastian.kolosa@gmail.com>
2016-12-19 21:20:12 -0500

    IOS-42 make vc

---
+++ Everest_iOS/UI/controllers/EventPageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EventPageViewController.swift
+//  Everest_iOS
+//
+//  Created by Sebastian Kolosa on 2016-12-19.
+//  Copyright © 2016 Everest. All rights reserved.
+//
+
+import UIKit
+
+class EventPageViewController: UIViewController {
+  private let headerAndStackViewContainer = HeaderAndStackViewContainer(withNavigationBar: false)
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let headerImageView = UIImageView()
+    let description
+  }
+}
